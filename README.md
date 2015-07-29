d36bcafaf6711f09ea663a25227c7d0dd07b68e1
Alexandr Kozin <al.kozin@gmail.com>
2015-07-29 15:58:31 +0600

    Script is now commit only changed files

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # iOS-Bump-Script
 
 Bash script to bump iOS app and push changes to git.
+
+Agvtool used to bump version.
+Then all the changed files are commit to Git.
+And push.
+
+You can see project name and build version in commit message.
+
+That's all.
--- bump.sh
+++ bump.sh
@@ -1,4 +1,30 @@
-agvtool bump -all
-git add *
-git commit -m "Bump"
-git push
+#!/bin/bash
+
+agvOutput=`agvtool bump -all`
+
+while read line
+do
+    [[ "$line" =~ .*\/\.\.\/(.*)\".* ]] &&
+    filePath=${BASH_REMATCH[1]}
+    if [ -n "$filePath" ]
+    then
+        git add "$filePath"
+    else
+        if [ -z "$projectName" ]
+        then
+            [[ "$line" =~ .*of\ project\ (.*)\ to.* ]] &&
+            projectName=${BASH_REMATCH[1]}
+            git add "$projectName.xcodeproj/project.pbxproj"
+        else
+            if [ -z "$buildVersion" ]
+            then
+            [[ "$line" =~ ([0-9]*)\. ]] &&
+            buildVersion=${BASH_REMATCH[1]}
+            fi
+        fi
+    fi
+done <<<"$agvOutput"
+
+git commit -m "Bump $projectName to $buildVersion"
+#git push
+exit 0
